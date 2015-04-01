e1c95db39c51d1f3f8f5285a7da7cd5b11cdf79a
revel8n <revel8n@gmail.com>
2015-03-31 21:19:52 -0500

    Initial support for USBGecko commands - Added additional access macros to PowerPC.h - Added support for readmem command - Added support for getregs command - Added support for pause/unfreeze commands - Added support for step command - Added support for status command - Added support for version command - Added initial stubs for remaining commands - Updated client thread logic to handle commands - Added logic for command thread processing

--- Source/Core/Core/HW/EXI_DeviceGecko.h
+++ Source/Core/Core/HW/EXI_DeviceGecko.h
@@ -23,15 +23,80 @@ public:
 	// Client for this server object
 	std::unique_ptr<sf::TcpSocket> client;
 	void ClientThread();
+	void CommandThread();
 	std::thread clientThread;
+	std::thread commandThread;
 	std::mutex transfer_lock;
 
 	std::deque<u8> send_fifo;
 	std::deque<u8> recv_fifo;
 
+	template<typename T>
+	bool read_data(T& data);
+	template<typename T>
+	bool read_data(std::vector<T>& data);
+	bool read_data(u8* data, u64 size);
+
+	template<typename T>
+	bool write_data(const T& data);
+	template<typename T>
+	bool write_data(const std::vector<T>& data);
+	bool write_data(const u8* data, u64 size);
+
+protected:
+
+	bool IsClientValid() const;
+
 private:
+	enum class WiiStatus : u8
+	{
+		Running,
+		Paused,
+		Breakpoint,
+		Loader,
+		Unknown
+	};
+
+	static const u32 packetsize = 0xF800;
+	static const u32 uplpacketsize = 0xF80;
+
+	static const u8 cmd_poke08 = 0x01;
+	static const u8 cmd_poke16 = 0x02;
+	static const u8 cmd_pokemem = 0x03;
+	static const u8 cmd_readmem = 0x04;
+	static const u8 cmd_pause = 0x06;
+	static const u8 cmd_unfreeze = 0x07;
+	static const u8 cmd_breakpoint = 0x09;
+	static const u8 cmd_breakpointx = 0x10;
+	static const u8 cmd_sendregs = 0x2F;
+	static const u8 cmd_getregs = 0x30;
+	static const u8 cmd_cancelbp = 0x38;
+	static const u8 cmd_sendcheats = 0x40;
+	static const u8 cmd_upload = 0x41;
+	static const u8 cmd_hook = 0x42;
+	static const u8 cmd_hookpause = 0x43;
+	static const u8 cmd_step = 0x44;
+	static const u8 cmd_status = 0x50;
+	static const u8 cmd_cheatexec = 0x60;
+	static const u8 cmd_nbreakpoint = 0x89;
+	static const u8 cmd_version = 0x99;
+
+	static const u8 GCBPHit = 0x11;
+	static const u8 GCACK = 0xAA;
+	static const u8 GCRETRY = 0xBB;
+	static const u8 GCFAIL = 0xCC;
+	static const u8 GCDONE = 0xFF;
+
+	static const u8 GCNewVer = 0x80;
+
+	static const u8 BPExecute = 0x03;
+	static const u8 BPRead = 0x05;
+	static const u8 BPWrite = 0x06;
+	static const u8 BPReadWrite = 0x07;
+
 	static int    client_count;
 	volatile bool client_running;
+	volatile bool process_commands;
 
 	// Only ever one server thread
 	static void GeckoConnectionWaiter();
